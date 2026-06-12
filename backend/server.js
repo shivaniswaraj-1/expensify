5d264e1211cd08a95245603398eb6a9249658380
Shivani Swaraj <shivaniswaraj02@gmail.com>
2026-06-12 19:52:55 +0530

    Fix CORS for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://expensify-xi.vercel.app",
+    ],
     credentials: true,
   })
 );
